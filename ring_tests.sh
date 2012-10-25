c6c795a0f7f19742d8ef1c1416581f1bc96062ff
mvictoras <mvictoras@gmail.com>
2012-10-25 13:35:54 -0500

    Fix

--- ring_tests.sh
+++ ring_tests.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # n = 2520
-./submit.sh -n 2520 -p 1 -k 1 -t 2d
-./submit.sh -n 2520 -p 2 -k 1 -t 2d
-./submit.sh -n 2520 -p 3 -k 1 -t 2d
-./submit.sh -n 2520 -p 4 -k 1 -t 2d
-./submit.sh -n 2520 -p 5 -k 1 -t 2d
-./submit.sh -n 2520 -p 6 -k 1 -t 2d
-./submit.sh -n 2520 -p 7 -k 1 -t 2d
+./submit.sh -n 2520 -p 1 -k 1 -t ring
+./submit.sh -n 2520 -p 2 -k 1 -t ring
+./submit.sh -n 2520 -p 3 -k 1 -t ring
+./submit.sh -n 2520 -p 4 -k 1 -t ring
+./submit.sh -n 2520 -p 5 -k 1 -t ring
+./submit.sh -n 2520 -p 6 -k 1 -t ring
+./submit.sh -n 2520 -p 7 -k 1 -t ring
 
 # n = 5040
-./submit.sh -n 5040 -p 1 -k 1 -t 2d
-./submit.sh -n 5040 -p 2 -k 1 -t 2d
-./submit.sh -n 5040 -p 3 -k 1 -t 2d
-./submit.sh -n 5040 -p 4 -k 1 -t 2d
-./submit.sh -n 5040 -p 5 -k 1 -t 2d
-./submit.sh -n 5040 -p 6 -k 1 -t 2d
-./submit.sh -n 5040 -p 7 -k 1 -t 2d
+./submit.sh -n 5040 -p 1 -k 1 -t ring
+./submit.sh -n 5040 -p 2 -k 1 -t ring
+./submit.sh -n 5040 -p 3 -k 1 -t ring
+./submit.sh -n 5040 -p 4 -k 1 -t ring
+./submit.sh -n 5040 -p 5 -k 1 -t ring
+./submit.sh -n 5040 -p 6 -k 1 -t ring
+./submit.sh -n 5040 -p 7 -k 1 -t ring
 
 # n = 10080
-./submit.sh -n 10080 -p 1 -k 1 -t 2d
-./submit.sh -n 10080 -p 2 -k 1 -t 2d
-./submit.sh -n 10080 -p 3 -k 1 -t 2d
-./submit.sh -n 10080 -p 4 -k 1 -t 2d
-./submit.sh -n 10080 -p 5 -k 1 -t 2d
-./submit.sh -n 10080 -p 6 -k 1 -t 2d
-./submit.sh -n 10080 -p 7 -k 1 -t 2d
+./submit.sh -n 10080 -p 1 -k 1 -t ring
+./submit.sh -n 10080 -p 2 -k 1 -t ring
+./submit.sh -n 10080 -p 3 -k 1 -t ring
+./submit.sh -n 10080 -p 4 -k 1 -t ring
+./submit.sh -n 10080 -p 5 -k 1 -t ring
+./submit.sh -n 10080 -p 6 -k 1 -t ring
+./submit.sh -n 10080 -p 7 -k 1 -t ring
